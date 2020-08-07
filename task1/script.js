f11f3b9dd2142140d3d9f968976224cba5c3e4e5
Khadija Ladan <khadijaladan67@gmail.com>
2020-08-07 09:38:53 +0100

    added share modal for mobile

--- task1/script.js
+++ task1/script.js
@@ -1,9 +1,15 @@
 const popUp = () => {
     var x = document.getElementById("share-mod");
+    var y = document.getElementById("footer");
+    var z = document.getElementById("share-fa");
     if (x.className === "share"){
         x.className += " popUp";
+        y.className += " close";
+        z.className += " opened";
     }
     else {
         x.className = "share";
+        y.className = "footer";
+        z.className = "icon-share";
     }
 }

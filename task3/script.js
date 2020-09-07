a359aecdf75680400e4636c75d995a99c8b2c77b
Khadija Ladan <khadijaladan67@gmail.com>
2020-09-07 09:36:30 +0100

    added function to create card with button click

--- task3/script.js
+++ task3/script.js
@@ -1,6 +1,6 @@
 const inputText = document.querySelector('#inputField');
-const shortenedLink = document.querySelector('#link-to-copy').innerText;
-const copyButton = document.querySelector('#copy-button');
+const inputCard = document.querySelector('#input-card');
+let i = 1;
 
 const isFilled = () => {
     if(!(inputText.length > 0)) {
@@ -8,7 +8,41 @@ const isFilled = () => {
     }
 }
 
+const createLink = () => {
+    var div = document.createElement("div");
+    var p1 = document.createElement("p");
+    var p2 = document.createElement("p");
+    var button = document.createElement("button");
+    var originalLink = document.createTextNode("https://www.frontendmentor.io");
+    var shortLink = document.createTextNode("https://rel.ink/k4IKyk");
+
+    div.classList.add("shortened-link-card")
+
+    p1.classList.add("original-link");
+    p1.appendChild(originalLink);
+
+    
+
+    p2.classList.add("shortened-link");
+    p2.id = "link-to-copy" + i;
+    p2.appendChild(shortLink)
+
+    button.classList.add("copy", "button");
+    button.id = "copy-button" + i;
+    button.addEventListener("click", copy)
+
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(button);
+
+    inputCard.after(div);
+    i++;
+}
+
 const copy = () => {
+    linkId = "link-to-copy" + i;
+    var shortenedLink = document.getElementById(linkId).innerText;
+    var copyButton = document.querySelector('#copy-button');
     var elem = document.createElement("textarea");
     document.body.appendChild(elem);
     elem.value = shortenedLink;

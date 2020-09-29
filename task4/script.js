75eff4c20774fcaf608e1b75aeee6a09aac13897
Khadija Ladan <khadijaladan67@gmail.com>
2020-09-29 22:23:45 +0100

    completed designs

--- task4/script.js
+++ task4/script.js
@@ -1,4 +1,4 @@
-let mymap = L.map('map-conatiner').setView([51.505, -0.09], 13);
+var mymap = L.map('map-container').setView([9.5834, 6.5633], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,5 +6,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1IjoibGFkYW4xMjM0NSIsImEiOiJjazI5amVkZ3EyYWduM2JtemMzc2c1MTA0In0.3uDS3OeJWkTkaVQE1yVd4g'
 }).addTo(mymap);
+
+var marker = L.divIcon({
+    html: '<i class="fas fa-map-marker-alt"></i>',
+    iconSize: [20, 20],
+    className: 'marker-icon'
+});
+
+
+var marker = L.marker([9.6034, 6.5633], {
+        icon: marker
+    }).addTo(mymap);

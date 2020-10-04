5b742eaeaca0de3f4c4f5a53a880b1dc9eabc4fa
Khadija Ladan <khadijaladan67@gmail.com>
2020-10-04 11:06:16 +0100

    adjusted map center

--- task4/script.js
+++ task4/script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map-container').setView([9.5834, 6.5633], 13);
+var mymap = L.map('map-container').setView([9.5990, 6.5633], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',

5a3dd2f3aaaffe118dc4d4ecd50a731d90f370b8
Khadija Ladan <khadijaladan67@gmail.com>
2020-10-09 23:00:29 +0100

    Added function to get local IP

--- task4/script.js
+++ task4/script.js
@@ -1,24 +1,3 @@
-var mymap = L.map('map-container').setView([9.5990, 6.5633], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibGFkYW4xMjM0NSIsImEiOiJjazI5amVkZ3EyYWduM2JtemMzc2c1MTA0In0.3uDS3OeJWkTkaVQE1yVd4g'
-}).addTo(mymap);
-
-var marker = L.divIcon({
-    html: '<i class="fas fa-map-marker-alt"></i>',
-    iconSize: [20, 20],
-    className: 'marker-icon'
-});
-
-var marker = L.marker([9.6034, 6.5633], {
-    icon: marker
-}).addTo(mymap);
-
 const InputField = document.querySelector('#search-input');
 const ipAddressField = document.querySelector('#ip-addess');
 const locationField = document.querySelector('#location');
@@ -26,16 +5,44 @@ const timeZoneField = document.querySelector('#time-zone');
 const ispField = document.querySelector('#isp');
 let localIp
 
-fetch('https://api.ipify.org?format=json')
+const getLocalApi = async () => {
+    res = await fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
-    .then(json => localIp = json.ip);
+    .then(json => getIpDetails(json.ip));
+addToCard(res);
+addMap(res.location.lat, res.location.lng);
+};
 
+const addMap = (lat, lng) => {
+    var mymap = L.map('map-container').setView([lat, lng], 13);
 
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoibGFkYW4xMjM0NSIsImEiOiJjazI5amVkZ3EyYWduM2JtemMzc2c1MTA0In0.3uDS3OeJWkTkaVQE1yVd4g'
+    }).addTo(mymap);
 
-const searchIp = async () => {
-    let response = await fetch('https://geo.ipify.org/api/v1?apiKey=at_LrBXguLHGJk0OUtLa2SlIUF2oYf50&ipAddress=' + InputField.value);
+    var marker = L.divIcon({
+        html: '<i class="fas fa-map-marker-alt"></i>',
+        iconSize: [20, 20],
+        className: 'marker-icon'
+    });
+
+    var marker = L.marker([lat, lng], {
+        icon: marker
+    }).addTo(mymap);
+};
+
+const getIpDetails = async (ip) => {
+    let response = await fetch('https://geo.ipify.org/api/v1?apiKey=at_LrBXguLHGJk0OUtLa2SlIUF2oYf50&ipAddress=' + ip);
     let res = await response.json();
+    return res;
+}
 
+const addToCard = (res) => {
     var ipAddress = document.createTextNode(res.ip);
     var location = document.createTextNode(`${res.location.city}, ${res.location.region}`);
     var timeZone = document.createTextNode(res.location.timezone);
@@ -50,4 +57,12 @@ const searchIp = async () => {
     locationField.appendChild(location);
     timeZoneField.appendChild(timeZone);
     ispField.appendChild(isp);
+}
+
+const searchIp = async () => {
+    res = await getIpDetails(InputField.value);
+    addToCard(res)
+    addMap(res.location.lat, res.location.lng)
 };
+
+getLocalApi();
